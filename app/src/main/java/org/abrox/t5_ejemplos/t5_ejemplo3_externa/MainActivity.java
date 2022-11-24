87f99a4775c3a5dd15ed9e77d90990ef855896d7
albrox9 <dam.albrox9@gmail.com>
2022-11-24 13:14:59 +0100

    Initial commit

---
+++ app/src/main/java/org/abrox/t5_ejemplos/t5_ejemplo3_externa/MainActivity.java
@@ -0,0 +1,19 @@
+package org.abrox.t5_ejemplos.t5_ejemplo3_externa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void cerrar(View v){
+        finish();
+    }
+}
